4c449a58682a61a6067a1179b6e4506c66923769
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-03-06 10:31:38 +0900

    Initial Commit

---
+++ MyOkashi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by NSPC201admin on 2024/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by NSPC201admin on 2024/03/06.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
