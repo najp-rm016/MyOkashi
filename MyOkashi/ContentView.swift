25ff44afee1331d77e7d08bd9a6974c5a75b4cc9
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-03-07 16:05:09 +0900

    福嶋さんにご確認頂くためにコミット

--- MyOkashi/ContentView.swift
+++ MyOkashi/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var okashiDataList = OkashiData()
+    @State var inputText = ""
+    @State var showSafari = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("キーワード", text: $inputText
+                      , prompt: Text("キーワードを入力してください"))
+            .onSubmit{
+                Task{
+                    await okashiDataList.searchOkashi(keyword: inputText)
+                }
+            }
+            
+            .submitLabel(.search)
+            .padding()
+            
+            List(okashiDataList.okashiList){ okashi in
+                Button(action: {
+                    showSafari.toggle()
+                }){
+                    HStack {
+                        AsyncImage(url: okashi.image){ image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 40)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        Text(okashi.name)
+                    }
+                }
+                
+                .sheet(isPresented: self.$showSafari, content: {
+                    SafariView(url: okashi.link)
+                        .edgesIgnoringSafeArea(.bottom)
+                })
+            }
         }
-        .padding()
     }
 }
 
